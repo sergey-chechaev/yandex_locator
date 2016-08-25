772f35c6f550375f3c6d357d9727c47a43a1d440
Sergey <sergey@2net.me>
2016-08-25 17:24:54 +0300

    add test for configuration

--- spec/yandex_locator_spec.rb
+++ spec/yandex_locator_spec.rb
@@ -5,8 +5,22 @@ describe YandexLocator do
     expect(YandexLocator::VERSION).not_to be nil
   end
 
-  it 'check default configuration' do
-    expect(YandexLocator.configuration.version).to eq "1.0"
+  context ".find_geo_lat_long_session" do
+
+    it 'check default configuration' do
+      expect(YandexLocator.configuration.version).to eq "1.0"
+    end
+
+    it 'check set new configuration value' do
+      YandexLocator.configure do |config|
+        config.api_key = "test_api_key"
+        config.version = "2.0"
+      end
+
+      expect(YandexLocator.configuration.api_key).to eq("test_api_key")
+      expect(YandexLocator.configuration.version).to eq("2.0")
+    end
+
   end
 
 end
